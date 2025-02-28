a3f870669f26f89df4b858b7cd573a1613a407b1
coderahulmishra <coderahulmishra@gmail.com>
2025-02-28 22:24:32 +0530

    now task store on localStorage

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const TaskInput = document.querySelector("#taskInput");
 const taskAddBtn = document.querySelector("#taskAddBtn");
-const taskFilter = document.querySelector("#taskFilter");
 const taskContainer = document.querySelector(".taskContainer");
 
+const taskArr = JSON.parse(localStorage.getItem("Task")) || [];
 
-function Task(value){
+const num = 4;
+const data = taskArr.find((el) => el.taskId === num);
+
+console.log(data);
+
+taskArr.map((el) => {
+    Task(el.taskValue,el.taskId)
+})
+
+function addObj(TaskData,id){
+    const obj = {
+        taskValue:TaskData,
+        taskId:id,
+        taskComplete:false
+    }
+    taskArr.push(obj);
+    localStorage.setItem("Task",JSON.stringify(taskArr));
+    console.log(taskArr);
+}
+
+
+function Task(value,ID){
     const Task = document.createElement("div");
     Task.classList.add("task","listContainer");
+    Task.dataset.taskId = ID;
     const checkBox = document.createElement("div");
     checkBox.classList.add("checkbox");
     const inputBox = document.createElement("input");
@@ -27,14 +49,27 @@ function Task(value){
     Task.appendChild(deleteBtn);
 
     checkBox.addEventListener("click",() => {
-        checkBox.parentElement.classList.toggle("complete");
+        // checkBox.parentElement.classList.toggle("complete");
+        const key = checkBox.parentElement.dataset.taskId;
+        const checkboxDetail = taskArr.find((el) => el.taskId == key);
+        if(!checkboxDetail.taskComplete){
+            checkboxDetail.taskComplete = true;
+            // checkBox.parentElement.classList.add("complete");
+            console.log(checkboxDetail);
+            localStorage.setItem("Task",JSON.stringify(taskArr));
+        }else{
+            checkboxDetail.taskComplete = false;
+            // checkBox.parentElement.classList.remove("complete");
+            console.log(checkboxDetail);
+            localStorage.setItem("Task",JSON.stringify(taskArr));
+        }
     })
 
     editBtn.addEventListener("click",() => {
         const editInput = inputBox.disabled = !inputBox.disabled;
         if(!editInput){
             inputBox.focus();
-            editBtn.textContent = "Save"
+            editBtn.textContent = "Save";
         }else{
             editBtn.textContent = "Edit";
         }
@@ -53,8 +88,9 @@ taskAddBtn.addEventListener("click",() => {
         console.log("Input Value is Empty");
     }else{
         Task(TaskInput.value)
+        addObj(TaskInput.value,taskArr.length+1)
         TaskInput.value = "";
-    }
+    }             
 })
 
 TaskInput.addEventListener("keydown",(e) => {
@@ -62,7 +98,7 @@ TaskInput.addEventListener("keydown",(e) => {
         if(!TaskInput.value){
             console.log("Input Value is Empty");
         }else{
-            Task(TaskInput.value);
+            Task(TaskInput.value); 
             TaskInput.value = "";
         }
     }
@@ -70,4 +106,8 @@ TaskInput.addEventListener("keydown",(e) => {
 
 taskFilter.addEventListener("change",(e) => {
     console.log(e.target.value);
+})
+
+document.addEventListener("keydown",(e) => {
+    console.log(e.key);
 })
